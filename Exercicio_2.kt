ccdef448e64059abfe149b0ebe05ba1cc7f17d74
RaphaelaSPereira <raphaelapereira006@gmail.com>
2024-03-19 08:11:28 -0300

    commit

---
+++ Exercicio_2.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>){
+
+    println("Digite o primeiro número: ")
+    val X = readLine()!!.toInt()
+
+    println("Digite o segundo número: ")
+    val Y = readLine()!!.toInt()
+
+    val resultado = multiplica(X, Y)
+
+    println("O resultado da multiplicação é = $resultado")
+}
+
+fun multiplica(X: Int, Y: Int): Int {
+    return X * Y
+
+}
